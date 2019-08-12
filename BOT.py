866bf7b2f3681e89f53e01cb52ff76e93d1f3f56
syobon <syobon_official@vivaldi.net>
2019-08-12 19:03:51 +0900

    help対応

--- BOT.py
+++ BOT.py
@@ -5,8 +5,17 @@ bot = commands.Bot(command_prefix='s!')
 
 # コマンド
 @bot.command()
+async def help(ctx, tohelp='all'):
+    if tohelp == 'all':
+        await ctx.send('現在使用できるコマンドは以下のとおりです。\r```\rsay   任意のテキストを送信します。現在空白は2つまで対応しています。\rcheck BOTの稼働を確認します。他のコマンドが使えないときにお試しください。\r```')
+    if tohelp == 'say':
+        await ctx.send('使用方法  `s!say 文字列`\r「文字列」で指定したテキストを送信します。')
+    if tohelp == 'check':
+        await ctx.send('使用方法  `s!check`\rBOTの稼働を確認します。他のコマンドが使えないときにお試しください。')
+
+@bot.command()
 async def say(ctx, first='テキストを入れてください', second='', third=''):
-    await ctx.send(first + second + third)
+    await ctx.send(first + ' ' + second + ' ' + third)
 
 @bot.command()
 async def check(ctx):
