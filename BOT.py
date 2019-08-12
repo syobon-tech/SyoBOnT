59155a92953f069130d167fa9dee8e92837c67f9
syobon <syobon_official@vivaldi.net>
2019-08-12 18:05:14 +0900

    first commit

---
+++ BOT.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+
+# 設定
+bot = commands.Bot(command_prefix='s!')
+
+# コマンド
+@bot.command()
+async def say(ctx, saytext : str):
+    await ctx.send(saytext)
+
+
+@bot.command()
+async def check(ctx):
+    await ctx.send("稼働中です")
+
+# Botの起動とDiscordサーバーへの接続
+bot.run('NjEwMzU3OTA3OTAwNDY1MTky.XVEJKg.ksWbKRLkGD3ucm41LZf5B05QznI')
